0392e19a0f7dca9124a76bbd18bc5ebca133cb48
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-16 10:51:52 +0300

    'complex_http_server_using_nodejs'

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,27 +3,27 @@ const http = require('http');
 const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DB = args[0];
+const DATABASE = args[0];
 
 const app = http.createServer(async (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-    const { url } = req;
+  const { url } = req;
 
-    if (url === '/') {
-        res.write('Hello Holberton School!');
-    } else if (url === '/students') {
-        res.write('This is the list of our students\n');
-        try {
-            const students = await countStudents(DB);
-            res.end(`${students.join('\n')}`);
-        } catch (error) {
-            res.end(error.message);
-        }
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
     }
-    res.statusCode = 404;
-    res.end();
+  }
+  res.statusCode = 404;
+  res.end();
 }).listen(1245);
 
 module.exports = app;
