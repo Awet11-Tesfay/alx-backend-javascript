1cef8f34ce151046e945fe87e85b05b77ef0414b
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-16 09:35:05 +0300

    'Reading_file_with_nodejs'

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  let data;
+  let content;
 
   try {
-    data = fs.readFileSync(path);
+    content = fs.readFileSync(path);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 
-  data = data.toString().split('\n');
+  content = content.toString().split('\n');
 
-  let students = data.filter((item) => item);
+  let students = content.filter((item) => item);
 
   students = students.map((item) => item.split(','));
 
   const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
   console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
 
-  const st_field = {};
+  const fields = {};
   for (const i in students) {
     if (i !== 0) {
-      if (!st_field[students[i][3]]) st_field[students[i][3]] = [];
+      if (!fields[students[i][3]]) fields[students[i][3]] = [];
 
-      st_field[students[i][3]].push(students[i][0]);
+      fields[students[i][3]].push(students[i][0]);
     }
   }
 
-  delete st_field.field;
+  delete fields.field;
 
-  for (const key of Object.keys(st_field)) {
+  for (const key of Object.keys(fields)) {
     console.log(
-      `Number of students in ${key}: ${st_field[key].length}. List: ${st_field[
+      `Number of students in ${key}: ${fields[key].length}. List: ${fields[
         key
       ].join(', ')}`,
     );
