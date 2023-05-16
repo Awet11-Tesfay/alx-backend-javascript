44db5c3c05f5b9844270414e42f813147680164b
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-16 11:00:21 +0300

    'small_express_server'

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 }).listen(1245);
 
 module.exports = app;
