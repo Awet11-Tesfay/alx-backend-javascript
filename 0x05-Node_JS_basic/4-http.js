57f636cb1e36eb0ba1751554789450172c27c9c1
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-16 10:06:20 +0300

    'create_server_using_http'

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const app = htt.createServer((req, res) => {
+const app = http.createServer((req, res) => {
     res.setHeader(200, {'Content-Type': 'text/html'});
     res.end('Hello Holberton School');
 }).listen(1245)
