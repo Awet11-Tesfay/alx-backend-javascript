adc4f7769a7db731c3726cfa4f350b33581b36ec
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-17 19:35:36 +0300

    'basic_mocha'

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,6 +1,9 @@
 function calculateNumber(a, b) {
-    const anum = Math.round(a);
-    const bnum = Math.round(b);
-
-    return anum + bnum;
-};
+    const aRound = Math.round(a);
+    const bRound = Math.round(b);
+  
+    return aRound + bRound;
+  }
+  
+  module.exports = calculateNumber;
+  
